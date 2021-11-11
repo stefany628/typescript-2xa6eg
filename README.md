b4a8b17ce38d1bebc5ba833efac5397b4192f7a7
stefany628 <sterfanymaria210@gmail.com>
2021-11-11 17:05:05 -0300

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# typescript-2xa6eg
+
+[Edit on StackBlitz ⚡️](https://stackblitz.com/edit/typescript-2xa6eg)
---
+++ index.ts
@@ -0,0 +1,5 @@
+/* Questão 2  Stefani*/
+const m1: number = +prompt('Nota1: ');
+const m2: number =+prompt('Nota2: ');
+let notafinal: number  = ( m1 * 4 + m2 * 6) / 10;
+document.write(notafinal);
